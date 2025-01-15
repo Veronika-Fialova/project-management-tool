e6e1166d8e017c5baa203d678b8d66f9d2618b97
[Veronika Fialova] <your email>
2025-01-15 10:54:34 +0100

    Icons, sidebar updated

--- client/src/app/dashboardWrapper.js
+++ client/src/app/dashboardWrapper.js
@@ -1,12 +1,30 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Navbar from "./components/Navbar"
-import Sidebar from './components/Sidebar';
+import Sidebar from './components/Sidebar'
+import StoreProvider, { useAppSelector } from "./redux"
 
 const DashboardWrapper = ( {children} ) => {
+
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
     return (
         <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
           <Sidebar />
-          <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+          <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+          isSidebarCollapsed ? "" : "md:pl-64"}`}>
             <Navbar />
             {children}
           </main>
@@ -14,4 +32,13 @@ const DashboardWrapper = ( {children} ) => {
       );
     };
 
-export default DashboardWrapper
+const AppWrapper = ( {children} ) => {
+    return (
+        <StoreProvider>
+          <DashboardWrapper>{children}</DashboardWrapper>
+        </StoreProvider>
+      );
+    };
+
+
+export default AppWrapper
--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import DashboardWrapper from "./dashboardWrapper";
+import AppWrapper from "./dashboardWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <DashboardWrapper>{children}</DashboardWrapper>
+        <AppWrapper>{children}</AppWrapper>
       </body>
     </html>
   );
